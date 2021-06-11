913db9ee5c2b86ff4ee591f9e25628ad7d0af368
Marcos Contreras <mcontreras@acl.cl>
2021-06-11 12:23:31 -0400

    props en react

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,25 +1,16 @@
-import React from "react";
+import { Component } from "react";
 
-const Form = () => {
-  return (
-    <div className="row justify-content-center">
-      <div className="form-group col-5">
-        <form>
-          <input
-            type="mail"
-            placeholder="&#xf0e0;   Correo Electronico"
-            className="form-control email mt-5"
-          />
-          <input
-            type="password"
-            placeholder="&#xf0e7;   Password"
-            className="form-control email mt-3"
-          />
-        </form>
-        <button className="btn btn-sm btn-warning w-100 mt-3">Enviar</button>
-      </div>
-    </div>
-  );
-};
-
-export default Form;
+// const Form = (props) => (
+//     <div className="col-6">
+//         Hola {props.name}
+//     </div>
+// )
+export default class Form extends Component {
+    render() { 
+        return ( 
+            <div>
+                Hola! {this.props.name} !!
+            </div>
+         );
+    }
+}
