0680f1f383f7459eba7d12d6b092447299f03bd4
Marcos Contreras <mcontreras@acl.cl>
2021-06-11 15:25:12 -0400

    cambios form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
 import Form from './components/Form';
 
+
 function App() {
   return (
     <div className="container">
-      <Form />
+      <Form 
+      input1 = 'Primer Nombre'
+      input2 = 'Apellido'
+      input3 = 'Correo Electronico'
+      input4 = 'Contraseña'
+      input5 = 'Dirección'
+      input6 = 'Ciudad'
+      input7 = 'Código Postal'
+      check1 = 'Estoy de Acuerdo con el registro de mis datos'
+      />
     </div>
   );
 }
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -6,11 +6,132 @@ import { Component } from "react";
 //     </div>
 // )
 export default class Form extends Component {
-    render() { 
-        return ( 
-            <div>
-                Hola! {this.props.name} !!
+  render() {
+    return (
+      <div>
+        <div className="row justify-content-center mt-5">
+          <div className="col-4">
+            <label>{this.props.input1}</label>
+            <div className="input-group my-3">
+              <div className="input-group-prepend">
+                <span className="input-group-text h-100">
+                  <i className="fa fa-user"></i>
+                </span>
+              </div>
+              <input
+                type="text"
+                className="form-control"
+                placeholder={"Ingrese " + this.props.input1 + "..."}
+              />
             </div>
-         );
-    }
+          </div>
+
+          <div className="col-4">
+            <label>{this.props.input2}</label>
+            <div className="input-group my-3">
+              <div className="input-group-prepend">
+                <span className="input-group-text h-100">
+                  <i className="fa fa-user"></i>
+                </span>
+              </div>
+              <input
+                type="text"
+                className="form-control"
+                placeholder={"Ingrese " + this.props.input2 + "..."}
+              />
+            </div>
+          </div>
+        </div>
+        <div className="row justify-content-center">
+          <div className="col-4">
+            <label>{this.props.input3}</label>
+            <div className="input-group my-3">
+              <div className="input-group-prepend">
+                <span className="input-group-text h-100">
+                  <i className="fa fa-user"></i>
+                </span>
+              </div>
+              <input
+                type="text"
+                className="form-control"
+                placeholder={"Ingrese " + this.props.input3 + "..."}
+              />
+            </div>
+          </div>
+
+          <div className="col-4">
+            <label>{this.props.input4}</label>
+            <div className="input-group my-3">
+              <div className="input-group-prepend">
+                <span className="input-group-text h-100">
+                  <i className="fa fa-user"></i>
+                </span>
+              </div>
+              <input
+                type="text"
+                className="form-control"
+                placeholder={"Ingrese " + this.props.input4 + "..."}
+              />
+            </div>
+          </div>
+        </div>
+        <div className="row justify-content-center">
+          <div className="col-8">
+            <label>{this.props.input5}</label>
+            <div className="input-group my-3">
+              <div className="input-group-prepend">
+                <span className="input-group-text h-100">
+                  <i className="fa fa-user"></i>
+                </span>
+              </div>
+              <input
+                type="text"
+                className="form-control"
+                placeholder={"Ingrese " + this.props.input5 + "..."}
+              />
+            </div>
+          </div>
+        </div>
+        <div className="row justify-content-start">
+          <div className="col-4 offset-2">
+            <label>{this.props.input6}</label>
+            <div className="input-group my-3">
+              <div className="input-group-prepend">
+                <span className="input-group-text h-100">
+                  <i className="fa fa-user"></i>
+                </span>
+              </div>
+              <input
+                type="text"
+                className="form-control"
+                placeholder={"Ingrese " + this.props.input6 + "..."}
+              />
+            </div>
+          </div>
+          <div className="col-2">
+            <label>{this.props.input7}</label>
+            <div className="input-group my-3">
+              <div className="input-group-prepend"></div>
+              <input type="text" className="form-control" />
+            </div>
+          </div>
+        </div>
+        <div className="row justify-content-center">
+            <div className="col-8">
+                <div className="form-check">
+                  <label className="form-check-label">
+                    <input type="checkbox" className="form-check-input" value="checkedValue" />
+                    {this.props.check1}
+                  </label>
+                </div>
+            </div>
+        </div>
+<div className="row justify-content-center">
+    <div className="col-8">
+    <button className="btn btn-primary my-3">Guardar</button>
+    </div>
+</div>
+      </div>
+    );
+  }
 }
